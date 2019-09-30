08a9c2e23867ab31d3ed65c1fa16568fcafdcad7
Shirosaya25 <Michaelzh2009@gmail.com>
2019-09-30 14:36:27 -0400

    test

--- project-2-spring-backend/src/main/java/com/revature/repositories/PlayerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.revature.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.revature.models.Player;
-
-public interface PlayerRepository extends JpaRepository<Player, Integer> {
-	
-}
