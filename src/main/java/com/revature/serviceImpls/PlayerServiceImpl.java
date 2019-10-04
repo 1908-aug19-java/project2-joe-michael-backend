248cc764819f8bf80380055c7b85bf91b0e0457e
jdg006 <jdg006@lvc.edu>
2019-10-03 20:06:02 -0400

    logging added

--- src/main/java/com/revature/serviceImpls/PlayerServiceImpl.java
+++ src/main/java/com/revature/serviceImpls/PlayerServiceImpl.java
@@ -1,6 +1,9 @@
 package com.revature.serviceImpls;
 
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.revature.models.Player;
@@ -10,32 +13,40 @@ import com.revature.services.PlayerService;
 @Service
 public class PlayerServiceImpl implements PlayerService {
 	
+	Logger logger = LoggerFactory.getLogger(PlayerService.class);
+	
+	
 	@Autowired 
 	private PlayerRepository playerRepository;
 	
 	@Override
 	public List<Player> findAllPlayers() {
+		logger.warn("all players pulled");
 		
 		return playerRepository.findAll();
 	}
 
 	@Override
 	public Player findPlayerById(Integer id) {
+		logger.warn("player pulled by id");
 		return playerRepository.getOne(id);
 	}
 
 	@Override
 	public Player addPlayer(Player player) {
+		logger.warn("player added");
 		return playerRepository.save(player);
 	}
 
 	@Override
 	public Player updatePlayer(Player player) {
+		logger.warn("player updated");
 		return playerRepository.save(player);
 	}
 
 	@Override
 	public Player deletePlayer(Player player) {
+		logger.warn("player deleted");
 		playerRepository.delete(player);
 		return player;
 	}
--- src/main/java/com/revature/serviceImpls/TeamServiceImpl.java
+++ src/main/java/com/revature/serviceImpls/TeamServiceImpl.java
@@ -1,6 +1,9 @@
 package com.revature.serviceImpls;
 
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.revature.models.Team;
@@ -9,6 +12,8 @@ import com.revature.services.TeamService;
 
 @Service
 public class TeamServiceImpl implements TeamService{
+	
+	Logger logger = LoggerFactory.getLogger(TeamService.class);
 
 	@Autowired 
 	private TeamRepository teamRepository;
@@ -16,26 +21,38 @@ public class TeamServiceImpl implements TeamService{
 	@Override
 	public List<Team> findAllTeams() {
 		
+		logger.warn("all teams pulled");
+		
 		return teamRepository.findAll();
 	}
 
 	@Override
 	public Team findTeamById(Integer id) {
+		logger.warn("team pulled by id");
+		
 		return teamRepository.getOne(id);
 	}
 
 	@Override
 	public Team addTeam(Team team) {
+		logger.warn("team added");
+		
 		return teamRepository.save(team);
 	}
 
 	@Override
 	public Team updateTeam(Team team) {
+		
+		logger.warn("team updated");
+		
 		return teamRepository.save(team);
 	}
 
 	@Override
 	public Team deleteTeam(Team team) {
+		
+		logger.warn("team deleted");
+		
 		teamRepository.delete(team);
 		return team;
 	}
